5739348927c64ddb8e45b2132673074ed93c3bc0
Arian Azemati <>
2018-10-08 10:22:49 -0400

    Initial commit for slide out menu project

---
+++ SlideOutMenuLBTA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideOutMenuLBTA
+//
+//  Created by Arian Azemati on 2018-10-08.
+//  Copyright © 2018 Arian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
